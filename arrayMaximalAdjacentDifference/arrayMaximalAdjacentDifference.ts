84474ac33dde12c8a89615ac1dd7780ff081012e
Yarden Lawson <yardenlawson@gmail.com>
2021-02-23 12:34:16 +0000

    arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,13 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  console.log(inputArray)
 
+  let arr = []
+
+  for (let i = 2; i < inputArray.length + 1; i++) {
+    arr.push(Math.abs(inputArray[i - 2] - inputArray[i - 1]))
+  }
+
+  return Math.max(...arr)
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));

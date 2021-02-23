7fedc267b7860ef6c41dc3dea55fb1dcdc11219d
Yarden Lawson <yardenlawson@gmail.com>
2021-02-23 12:25:58 +0000

    arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,22 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+  console.log(inputArray)
+  console.log(k)
+
+  let arr = []
+
+  for (let i = 2; i < inputArray.length + 1; i++) {
+    arr.push(inputArray[i - 2] + inputArray[i - 1])
+  }
+
+  console.log(arr)
+
+  return Math.max(...arr)
 
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+
+
+// for (let i=2; i < inputArray.length + 1; i+=2 ) {
+//   arr.push(inputArray[i - 2] + inputArray[i - 1])
+// }

cc9b434125a3de02989b538c41021df57f9ea632
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 19:10:45 +0000

    update

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,17 @@
-export function firstDigit(inputString: string): string {
+import { parse } from "@babel/core"
 
+export function firstDigit(inputString: string): string {
+  console.log(inputString)
+  const strArr = inputString.split('')
+  console.log(strArr)
+  for (let i = 0; i < strArr.length; i++) {
+    console.log(parseInt(strArr[i]))
+    if (parseInt(strArr[i]) || parseInt(strArr[i]) === 0) {
+      return strArr[i]
+    }
+  }
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));

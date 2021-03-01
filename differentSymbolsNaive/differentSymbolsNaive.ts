9853e2813546165530480eb2e314c8fc1e9a02ca
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 12:50:44 +0000

    update

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,15 @@
 export function differentSymbolsNaive(s: string): number {
+  console.log(s)
+
+  let sToArr = s.split('').sort()
+  let x = [...new Set(sToArr)]
+
+  console.log(x)
+
+  return x.length
+
+  
 
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));

aa41f77b22936cd5946d692dea89cbffd240d36c
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 14:18:54 +0000

    update

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,16 @@
 export function factorializeANumber(num: number): number {
+  console.log(num)
+  let numArr = []
+
+  for (let i = 1; i <= num; i++) {
+    numArr.push(i)
+  }
+  
+  console.log(numArr)
+
+  return numArr.reduce((curr, acc) => curr * acc)
 
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));  
+console.log(factorializeANumber(10));

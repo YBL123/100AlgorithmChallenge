701f2578770014396c0e5dc5fcc40f446dfce895
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 13:29:24 +0000

    update

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,10 @@
 export function evenDigitsOnly(n: number): boolean {
+  const arr = n.toString().split('')
+  console.log(arr)
+
+  return arr.every(num => parseInt(num) % 2 === 0)
 
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));

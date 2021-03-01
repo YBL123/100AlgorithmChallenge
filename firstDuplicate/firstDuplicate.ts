03b096304d6580ba2648bde3bcb0c2a88756e635
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 19:30:44 +0000

    update

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,15 @@
 export function firstDuplicate(a: number[]): number {
+  let found = {};
 
+  for (let i = 0; i < a.length; i++) {
+    if (found[a[i]] !== undefined) return a[i];
+    else {
+      found[a[i]] = i;
+    }
+  }
+
+  return -1;
 }
 
-// console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-// console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 4, 3, 5, 1]));

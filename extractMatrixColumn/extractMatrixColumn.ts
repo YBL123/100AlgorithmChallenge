835bf3f3bab903d652b8d541fdea4b2adbce5047
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 14:10:34 +0000

    update

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,12 @@
 export function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+  console.log(matrix)
+  let arr = []
+  let resArr = []
+  const subArr = matrix.forEach((subArr, i) => {
+    console.log(subArr[column])
+    arr.push(subArr[column])
+  })
+  return arr
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));

6738d3c66c2752a184cf9aca7afe3e3b7cc8af32
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 18:36:58 +0000

    update

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,16 +1,23 @@
 export function findClosestPair(numbers: number[], sum: number): number {
-  console.log(numbers)
-  console.log(sum)
+  let distance = -1
 
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < sum; i++) {
-      // if (numbers[i] + numbers[i + 1] === sum) {
-      //   console.log('hi')
-      // }
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = i + 1; j < numbers.length - 1; j++) {
+      const distanceSum = numbers[i] + numbers[j]
+      const absoluteDistance = Math.abs(i - j)
+
+      if (sum === distanceSum) {
+        if (distance === -1 || absoluteDistance < distance) {
+          distance = absoluteDistance
+        }
+      }
     }
-    
   }
+
+  return distance
+
 }
 
+
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-// console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([2, 3, 7], 8));

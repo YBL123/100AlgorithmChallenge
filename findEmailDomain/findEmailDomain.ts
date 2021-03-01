7fe3d54fd879548db6fba57a8b243832609d0d44
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 19:01:23 +0000

    update

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,8 @@
 export function findEmailDomain(address: string): string {
-
+  console.log(address)
+  const last = address.lastIndexOf('@')
+  return address.slice(last + 1, address.length)
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));

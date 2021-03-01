00a254c815aea0ab7e5699a14896e713150026d8
Yarden Lawson <yardenlawson@gmail.com>
2021-03-01 13:23:39 +0000

    update

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,20 @@
 export function domainType(domains: string[]): string[] {
+  console.log(domains)
+
+  const res = domains.map(el => {
+    if (el.includes('.org')) {
+      return 'organization'
+    } else if (el.includes('.com')) {
+      return 'commercial'
+    } else if (el.includes('.net')) {
+      return 'network'
+    } else if (el.includes('.info')) {
+      return 'information'
+    }
+  })
+
+  return res
 
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));

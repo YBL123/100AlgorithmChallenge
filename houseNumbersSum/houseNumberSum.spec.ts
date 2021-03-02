6d7f4459b39099d451ebe3ae153c2ae2755f07a8
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 12:08:41 +0000

    update

--- houseNumbersSum/houseNumberSum.spec.ts
+++ houseNumbersSum/houseNumberSum.spec.ts
@@ -1,6 +1,6 @@
 import { houseNumbersSum } from './houseNumbersSum';
 
-describe(houseNumbersSum.name, () => {
+xdescribe(houseNumbersSum.name, () => {
     it('Test 1', () => {
         // arrange
         const data = [5, 1, 2, 3, 0, 1, 5, 0, 2];
--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -2,5 +2,5 @@ export function houseOfCats(legs: number): number[] {
 
 }
 
-// console.log(houseOfCats(6));
+console.log(houseOfCats(6));
 // console.log(houseOfCats(2));

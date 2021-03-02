32eb9a049b7f1dbb4aedc6d84d2ccbf7815fee18
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 20:15:45 +0000

    update

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,11 @@
 export function isTandemRepeat(inputString: string): boolean {
 
+  const mid = inputString.length / 2
+
+  return inputString.slice(0, mid) === inputString.slice(mid, inputString.length)
+
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))

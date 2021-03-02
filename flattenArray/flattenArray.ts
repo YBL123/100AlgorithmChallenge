85a18c6b6d559a49984ad0d78e154ef5a5ba6402
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 11:41:06 +0000

    update

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,18 @@
 export function flattenArray(arr: any[]): any[] {
+  const result = []
+
+  arr.forEach((el) => {
+    if (Array.isArray(el)) {
+      result.push(...flattenArray(el))
+    } else {
+      result.push(el)
+    }
+  })
+  
+  return result
+
 
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[['a']], [['b']]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));

706628110d2faa360174a0d5a2aaec17f9291ce2
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 11:15:49 +0000

    update

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,13 @@
 export function firstNotRepeatingCharacter(s: string): string {
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (s.indexOf(char) == i && s.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return '_';
 
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter('abacabad'));
+console.log(firstNotRepeatingCharacter('abacabaabacaba'));

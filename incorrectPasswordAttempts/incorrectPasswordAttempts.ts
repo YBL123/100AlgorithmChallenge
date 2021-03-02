d0d20872e08b41485167f5cd18ba8c43d6b42f86
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 17:00:49 +0000

    update

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,20 @@
 export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+  console.log(passcode)
+  console.log(attempts)
+  // loop
+  // count -> to 10 consecutive if does not include passcode
+  // if it reaches 10 conescutive -> true
+  // if not false
 
+  let count = 0
+
+  for (let i = 0; i < attempts.length; i++) {
+    console.log(attempts[i])
+    if (attempts[i] !== passcode) {
+      count++
+    }
+  }
+  return count = 10 ? true : false
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));

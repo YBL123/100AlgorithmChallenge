d76ba549071bf5f181bec77b1ecefcbef44aeb31
Yarden Lawson <yardenlawson@gmail.com>
2021-03-02 20:10:55 +0000

    update

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,19 @@
+import { visitNode } from "typescript"
+
 export function isLucky(n: number): boolean {
+  let arr = n.toString().split('')
+
+  const midPointIndex = arr.length / 2
+
+  const firstHalf = arr.slice(0, midPointIndex)
+  const first = firstHalf.reduce((acc, curr) => parseInt(acc) + parseInt(curr))
+
+  const secondHalf = arr.slice(midPointIndex, arr.length)
+  const second = secondHalf.reduce((acc, curr) => parseInt(acc) + parseInt(curr))
+
+  return first === second
 
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230));
+console.log(isLucky(239017));

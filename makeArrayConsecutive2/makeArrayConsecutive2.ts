de364f62d9247bb2242b7d7ad5558421d445dd92
Yarden Lawson <yardenlawson@gmail.com>
2021-03-03 11:43:15 +0000

    update

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,16 @@
 export function makeArrayConsecutive2(statues: number[]): number {
 
+  let count = 0;
+
+  const sorted = statues.sort();
+
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i] !== sorted[i] + 1) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));

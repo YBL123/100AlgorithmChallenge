5bb7f9f3ccdc517aab10aaba27fd0910e6b31786
Yarden Lawson <yardenlawson@gmail.com>
2021-03-03 00:54:38 +0000

    update

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,20 @@
 export function longestDigitsPrefix(inputString: string): string {
 
+  const arr = inputString.split('')
+  let resArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    const x = parseInt(arr[i])
+    if (!x) {
+      var endPoint = arr.indexOf(arr[i])
+      console.log(endPoint)
+    }
+    resArr.push(...arr.splice(0, endPoint))
+  }
+  
+    return resArr.join('')
+
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));
+

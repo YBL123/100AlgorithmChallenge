1fdd959af0bac192969ab8c9fc08209c9aa49630
Yarden Lawson <yardenlawson@gmail.com>
2021-03-04 15:57:42 +0000

    update

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,9 @@
 export function properNounCorrection(noun: string): string {
+  const lower = noun.toLowerCase()
+  const upper = lower[0].toUpperCase()
 
+  return lower.replace(lower[0], upper)
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));

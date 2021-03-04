c651557f1058dc97a66b36db1aac11a01b241c49
Yarden Lawson <yardenlawson@gmail.com>
2021-03-04 16:35:40 +0000

    update

--- seekAndDestroy/seekAndDestroy.ts
+++ seekAndDestroy/seekAndDestroy.ts
@@ -1,6 +1,10 @@
 export function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
+  console.log(arr1)
+  console.log(arr2)
+
+  return arr1.filter(el => !arr2.includes(el))
 
 }
 
-// console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
-// console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
+console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
+console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));

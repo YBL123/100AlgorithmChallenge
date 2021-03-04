c50b9112899cf461df766c34cd940fbc346bbb6e
Yarden Lawson <yardenlawson@gmail.com>
2021-03-04 12:23:37 +0000

    update

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,9 @@
 export function maxMultiple(divisor: number, bound: number): number {
+  console.log(bound)
+  console.log(bound % divisor)
+  console.log(bound - (bound % divisor))
+  return bound - (bound % divisor)
 
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3,10));

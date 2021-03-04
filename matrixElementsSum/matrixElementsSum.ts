03a20f993c1f917e629315bc3c706bf5659d951b
Yarden Lawson <yardenlawson@gmail.com>
2021-03-04 12:14:57 +0000

    update

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,23 +1,35 @@
 export function matrixElementsSum(matrix: any[][]): number {
-  console.log(matrix)
-  let arr = [...matrix]
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0 || arr[i - 1] === 0) {
-      arr.pop(arr[i])
+  let priceTotal = 0
+  let ignoreArr = []
+
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i])
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0 ) {
+        ignoreArr.push(j)
+      } else if (ignoreArr.indexOf(j) === -1) {
+        priceTotal += matrix[i][j]
+      }
     }
-    
-  }
-
-  console.log(arr)
-
 }
 
+  return priceTotal
+
+
+}
 console.log(matrixElementsSum(
     [[0, 1, 1, 2],
     [0, 5, 0, 0],
     [2, 0, 3, 3]]
 ));
 
+// [0, 1, 1, 2],   
+// [0, 5, 0, 0], 
+// [2, 0, 3, 3]
+
+// [x, 1, 1, 2], 
+// [x, 5, x, x], 
+// [x, x, x, x]
 
 // any 0 OR located anywhere below a 0 in the same column is not considered suitable

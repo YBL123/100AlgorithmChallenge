427e1370e0518388f8d77d594fc5855c87f7da4c
Yarden Lawson <yardenlawson@gmail.com>
2021-03-05 13:52:13 +0000

    update

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,24 @@
 export function sumOddFibonacciNums(num: number): number {
 
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
 }
 
-// console.log(sumOddFibonacciNums(1000));
-// console.log(sumOddFibonacciNums(4000000));
-// console.log(sumOddFibonacciNums(10));
+
+
+console.log(sumOddFibonacciNums(1000));
+console.log(sumOddFibonacciNums(4000000));
+console.log(sumOddFibonacciNums(10));
+
+
+// return the sum of all odd Fibonacci numbers that are less than or equal to num

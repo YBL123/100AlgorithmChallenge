2e3cff28b989ed141390dfb48a2b9c0e705d21b0
Yarden Lawson <yardenlawson@gmail.com>
2021-03-05 13:57:17 +0000

    update

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,20 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
+  console.log(a);
+  console.log(b);
+  console.log(v);
 
+  for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+    for (let j = 0; j < b.length; j++) {
+      console.log(b[j]);
+      console.log(a[i] + b[j]);
+      if (a[i] + b[j] === v) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));

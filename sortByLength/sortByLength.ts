854f7462d2616313fdadbedf8fcff667ae8317f1
Yarden Lawson <yardenlawson@gmail.com>
2021-03-05 11:36:28 +0000

    update

--- sortByLength/sortByLength.ts
+++ sortByLength/sortByLength.ts
@@ -1,9 +1,16 @@
 export function sortByLength(inputArray: string[]): string[] {
 
+  return inputArray.sort((a, b) => a.length - b.length)
+
 }
 
-// console.log(sortByLength(["abc",
-// "",
-// "aaa",
-// "a",
-// "zz"]));
+console.log(sortByLength(["abc",
+"",
+"aaa",
+"a",
+"zz"]));
+
+
+
+// Given an array of strings, sort them in the order of increasing lengths. 
+// If two strings have the same length, their relative order must be the same as in the initial array.

13a0f7e05aa60ebc05bde51f2a5fccfa4e3dd1cd
Yarden Lawson <yardenlawson@gmail.com>
2021-03-05 14:34:01 +0000

    update

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,13 @@
 export function validTime(time: string): boolean {
 
+  return Number(time.split(':')[0]) < 24 && Number(time.split(':')[1]) < 60
+
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
+
+
+
+// given string is a correct time representation of the 24-hour clock.
